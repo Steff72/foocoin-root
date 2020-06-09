f824e30300a9b15869a5636f44d7039c30532617
Steff72 <>
2020-06-09 16:05:46 +0200

    blockchain display updated

--- frontend/src/components/Blockchainold.js
+++ frontend/src/components/Blockchainold.js
@@ -12,17 +12,10 @@ import chain from '../assets/chain.png'
 const Blockchain = () => {
     const [blockchain, setblockchain] = useState([])
     const [length, setlength] = useState(0)
-    const [slice, setslice] = useState(0)
-
-    const start = 0 * RANGE
-    const end = (slice+1) * RANGE
 
     const fetchPage = ({ start, end }) => {
         backend.get(`/blockchain/page?start=${start}&end=${end}`)
             .then(response => setblockchain(response.data))
-
-            
-        setslice(slice+1)
     }
 
     useEffect(() => {
@@ -30,22 +23,12 @@ const Blockchain = () => {
 
         backend.get('/blockchain/length')
             .then(response => setlength(response.data))
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-
-    const MoreButton = () => {
-        if (slice<length/RANGE) {
-            return (
-                <Button size="sm" variant="outline-light" className="mt-2"
-                onClick={() => {
-                    fetchPage({ start, end })
-                }}>Show more</Button>
-            )
-        }
-        return <div className="end">End of the FooChain</div>
+    const buttons = []
+    for (let i=0; i<length/RANGE; i++) {
+        buttons.push(i)
     }
-
 
     return (
         <div className="Blockchain">
@@ -53,13 +36,28 @@ const Blockchain = () => {
             <h3>FooChain</h3>
             <div>
                 {blockchain.map((block, idx) => (
-                    <div key={idx}>
-                        <Block block={block} />
+                    <div>
+                        <Block key={idx} block={block} />
                         <img className='Chain' src={chain} alt="chain" />
                     </div>
                 ))}
             </div>
-            <MoreButton />
+            <div>
+                {
+                    buttons.map(button => {
+                        const start = button * RANGE
+                        const end = (button+1) * RANGE
+                        
+                        return (
+                            <span key={button} onClick={() => fetchPage({ start, end })}>
+                                <Button size="sm" variant="outline-light" className="mt-2">
+                                    {button+1}
+                                </Button>{' '}
+                            </span>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
